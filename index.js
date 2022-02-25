a5cd945e19d1590bba2aad3d5905bb9227d51779
Jason <92811609+Mister-Zeng@users.noreply.github.com>
2022-02-24 22:20:16 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+let adviceNums = document.querySelector('.adviceNums');
+const dice = document.querySelector('.generator');
+let adviceText = document.querySelector('.adviceText');
+
+const fetchQuote = async () => {
+    try {
+        const res = await fetch('https://api.adviceslip.com/advice')
+        const data = await res.json();
+        adviceText.innerText = data.slip.advice;
+        adviceNums.innerText = `A D V I C E #${data.slip.id}`;
+    } catch (e) {
+        adviceText.innerText = 'Error';
+    }
+};
+
+dice.addEventListener('click', () => fetchQuote());
